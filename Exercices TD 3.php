3daa34824162a8029746bb4809a6c4145152a9b9
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2019-05-02 10:17:32 +0200

    Exercices du troisième TD 02/05/2019

---
+++ Exercices TD 3.php
@@ -0,0 +1,71 @@
+<!-- Calculatrice -->
+<!-- HTML -->
+<form action="traitement.php" method="POST">
+			<p>Nombre 1: <input type="text" name="nombre1"></p>
+			<p>Nombre 2: <input type="text" name="nombre2"></p>
+			<p>
+				<select name="operande">
+				<option value="+">Addition</option>
+				<option value="-">Soustraction</option>
+				<option value="*">Multiplication</option>
+				<option value="/">Division</option>
+				</select>
+			</p>
+			<p><input type="submit" name="Calcul"></p>
+		</form>
+
+<!-- PHP -->
+<?php 
+			$nombre1 = $_POST['nombre1'];
+			$nombre2 = $_POST['nombre2'];
+			$operande = $_POST['operande'];
+
+			switch ($operande) {
+				case "+":
+					$resultat = $nombre1+$nombre2;
+					break;
+				case "-":
+					$resultat = $nombre1-$nombre2;
+					break;
+				case "*":
+					$resultat = $nombre1*$nombre2;
+					break;
+				case "/":
+					$resultat = $nombre1/$nombre2;
+					break;
+			}
+
+			echo $nombre1.''.$operande.''.$nombre2.'='.$resultat;
+		 ?>
+
+
+<!-- Table de multiplication -->
+<!-- HTML -->
+<form action="traitement.php" method="POST">
+			<p>Nombre A: <input type="text" name="nombreA"></p>
+			<p>Nombre de lignes: <input type="text" name="nombreB"></p>
+			<p><input type="submit" name="Calcul"></p>
+</form>
+
+<!-- PHP -->
+<?php 
+			/* Table de multiplication par 9 */
+			$nombreA = $_POST['nombreA'];
+			$nombreB = $_POST['nombreB'];
+			$multiplication = 0;
+			$i = 1;
+			echo '<style type=text/css>
+   			table tbody tr:nth-child(odd) { background-color: red; }
+			</style>';
+			echo '<table border="1">';
+			echo '<td>';
+			while ($i <= $nombreB)
+			{
+				$multiplication = $i * $nombreA;
+				echo '<tr><td>'.$i.' x '.$nombreA.' =</td>';
+				echo '<td>'.$multiplication.'</tr>';
+				$i++;
+			}
+			echo '</td>';
+			echo '</table>';
+		 ?>
