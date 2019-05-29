a718617aea87d83dde456702dfdd5a446b287cef
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2019-05-29 11:34:54 +0200

    Exercices 1 et 2

--- ExercicesTD7.php
+++ ExercicesTD7.php
@@ -9,4 +9,17 @@
 		//appel de ma fonction 
 		$v = calculVolume(4, 2, 2); 
 		echo 'Le volume est de ' . $v;
-	?>
+?>
+
+<!-- Exercice 2 -->
+<?php 
+	function moyenne($nombre1, $nombre2) 
+	{ 
+		$moyenne = $nombre1 + $nombre2 / 2; 
+		return $moyenne; 
+	} 
+
+	//appel de ma fonction 
+	$m = moyenne(4, 2); 
+	echo 'La moyenne est de ' . $m;
+?>
