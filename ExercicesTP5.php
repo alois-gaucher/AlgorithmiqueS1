3a8b3d09826933688c203b05131e1a9132b0bf26
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2019-06-05 18:22:46 +0200

    Update ExercicesTP5.php

--- ExercicesTP5.php
+++ ExercicesTP5.php
@@ -10,9 +10,10 @@
 
 <!-- Exercice Upload -->
 <!-- HTML -->
+<body>
 	<!-- Form Upload -->
 	<form action="index.php" method="post" enctype="multipart/form-data">
-	    Select image to upload:
+	    Sélectionnez une image à uploader:
 	    <input type="file" name="fileToUpload" id="fileToUpload">
 	    <input type="submit" value="Upload Image" name="submit">
 	</form>
@@ -29,11 +30,12 @@
 		if(isset($_POST["submit"])) {
 		    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 		    if($check !== false) {
-		        echo "File is an image - " . $check["mime"] . ".";
+		        echo "Le fichier est une image - " . $check["mime"] . ".";
 		        $uploadOk = 1;
 		    } else {
-		        echo "File is not an image.";
+		        echo "Le fichier n'est pas une image.";
 		        $uploadOk = 0;
 		    }
 		}
+		echo '<img src="uploads/'.$target_file.'">';
 ?>
